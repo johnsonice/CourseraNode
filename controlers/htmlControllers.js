841a287799da6aa667750bf703c79e59ee0eedf8
Chengyu Huang <chengyuhuang16@gmail.com>
2016-04-11 14:36:10 -0400

    save updates

---
+++ controlers/htmlControllers.js
@@ -0,0 +1,15 @@
+//load dependencies
+var bodyParser = require('body-parser');
+
+module.exports = function(app){
+    app.get("/testing",function(req,res){});
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
