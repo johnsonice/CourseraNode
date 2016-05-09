c7263e915b483b31ca83172cc4c6f23889991cff
Chengyu Huang <chengyuhuang16@gmail.com>
2016-05-09 16:34:22 -0400

    basic auth

--- app.js
+++ app.js
@@ -21,6 +21,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//authorization 
+//basic
+function auth(req,res,next){
+    //console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if(!authHeader){
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer(authHeader.split(' ')[1],
+    'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        next();
+    }else{
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+        return;
+    }
+}
+app.use(auth);
+app.use(function(err,req,res,next){
+    res.writeHead(err.status ||500,{
+        'WWW-Authenticate': 'Basic',
+        'Content-Type': 'text/plain'
+    });
+    res.end(err.message);
+});
 
 
 
